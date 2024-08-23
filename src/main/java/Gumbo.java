888e1322a5094b471b1f6ba2428bbba2d1a092d1
yikjunxian <>
2024-08-23 15:48:27 +0800

    Add support for list

--- src/main/java/Gumbo.java
+++ src/main/java/Gumbo.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Gumbo {
@@ -6,13 +8,21 @@ public class Gumbo {
                 "What can I do for you?\n");
 
         Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        ArrayList<String> strArr = new ArrayList<>();
+
         while (true) {
             String userInput = myObj.nextLine();  // Read user input
             if (userInput.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else if (userInput.equals("list")) {
+                int i = strArr.size();
+                for (int j = 1; j < i+1; j++) {
+                    System.out.println(j + ". " + strArr.get(j - 1));
+                }
             } else {
-                System.out.println(userInput);
+                strArr.add(userInput);
+                System.out.println("Added: " + userInput);
             }
         }
     }
