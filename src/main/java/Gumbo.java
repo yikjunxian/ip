2bfe6f5222c852141c75995c80acb9ae3e526d28
yikjunxian <>
2024-08-24 02:26:38 +0800

    Added automated text UI testing

--- src/main/java/Gumbo.java
+++ src/main/java/Gumbo.java
@@ -93,19 +93,19 @@ public class Gumbo {
             } else if (userInput.equals("list")) {
                 int i = taskArr.size();
                 System.out.println("Here are the tasks in your list:\n");
-                for (int j = 1; j < i+1; j++) {
+                for (int j = 1; j < i + 1; j++) {
                     Task x = taskArr.get(j - 1);
                     System.out.println(j + ". " + x);
                 }
             } else if (userInput.startsWith("mark")) {
-                int taskNum =  Character.getNumericValue(userInput.charAt(5));
-                Task completedTask = taskArr.get(taskNum-1);
+                int taskNum = Character.getNumericValue(userInput.charAt(5));
+                Task completedTask = taskArr.get(taskNum - 1);
                 completedTask.markAsDone();
                 System.out.println("Nice! I've marked this task as done:\n"
-                                    + completedTask);
+                        + completedTask);
             } else if (userInput.startsWith("unmark")) {
-                int taskNum =  Character.getNumericValue(userInput.charAt(7));
-                Task completedTask = taskArr.get(taskNum-1);
+                int taskNum = Character.getNumericValue(userInput.charAt(7));
+                Task completedTask = taskArr.get(taskNum - 1);
                 completedTask.markAsUndone();
                 System.out.println("OK, I've marked this task as not done yet:\n"
                         + completedTask);
@@ -115,8 +115,8 @@ public class Gumbo {
                 Deadline newDeadline = new Deadline(userInput.substring(0, dlIndex - 1), by);
                 taskArr.add(newDeadline);
                 System.out.println("Got it. I've added this task:\n"
-                                    + newDeadline + "\n"
-                                    + "Now you have " + taskArr.size() + " tasks in the list");
+                        + newDeadline + "\n"
+                        + "Now you have " + taskArr.size() + " tasks in the list");
             } else if (userInput.startsWith("todo")) {
                 Todo newTodo = new Todo(userInput.substring(5));
                 taskArr.add(newTodo);
@@ -127,13 +127,13 @@ public class Gumbo {
                 int fromIndex = userInput.lastIndexOf("/from");
                 int toIndex = userInput.lastIndexOf("/to");
                 Event newEvent = new Event(userInput.substring(6, fromIndex - 1),
-                                        userInput.substring(fromIndex + 6, toIndex - 1),
-                                        userInput.substring(toIndex + 4));
+                        userInput.substring(fromIndex + 6, toIndex - 1),
+                        userInput.substring(toIndex + 4));
                 taskArr.add(newEvent);
                 System.out.println("Got it. I've added this task:\n"
                         + newEvent + "\n"
                         + "Now you have " + taskArr.size() + " tasks in the list");
-        } else {
+            } else {
                 taskArr.add(new Task(userInput));
                 System.out.println("Added: " + userInput);
             }
