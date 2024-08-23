7612b20aa8652b786f6bd57a708be456f9a46505
yikjunxian <>
2024-08-23 16:18:36 +0800

    Add support for mark as done and undone

--- src/main/java/Gumbo.java
+++ src/main/java/Gumbo.java
@@ -3,12 +3,41 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Gumbo {
+
+    public static class Task {
+        protected String description;
+        protected boolean isDone;
+
+        public Task(String description) {
+            this.description = description;
+            this.isDone = false;
+        }
+
+        public String getStatusIcon() {
+            return (isDone ? "X" : " "); // mark done task with X
+        }
+
+        public void markAsDone() {
+            this.isDone = true;
+        }
+
+        public void markAsUndone() {
+            this.isDone = false;
+        }
+
+        @Override
+        public String toString() {
+            return this.description;
+        }
+
+    }
     public static void main(String[] args) {
         System.out.println("Hello! I'm Gumbo\n" +
                 "What can I do for you?\n");
 
         Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-        ArrayList<String> strArr = new ArrayList<>();
+        ArrayList<Task> taskArr = new ArrayList<>();
+
 
         while (true) {
             String userInput = myObj.nextLine();  // Read user input
@@ -16,12 +45,26 @@ public class Gumbo {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
             } else if (userInput.equals("list")) {
-                int i = strArr.size();
+                int i = taskArr.size();
+                System.out.println("Here are the tasks in your list:\n");
                 for (int j = 1; j < i+1; j++) {
-                    System.out.println(j + ". " + strArr.get(j - 1));
+                    Task x = taskArr.get(j - 1);
+                    System.out.println(j + ". " + "[" + x.getStatusIcon() + "] " + x);
                 }
+            } else if (userInput.startsWith("mark")) {
+                int taskNum =  Character.getNumericValue(userInput.charAt(5));
+                Task completedTask = taskArr.get(taskNum-1);
+                completedTask.markAsDone();
+                System.out.println("Nice! I've marked this task as done:\n"
+                                    + " [X] " + completedTask);
+            } else if (userInput.startsWith("unmark")) {
+                int taskNum =  Character.getNumericValue(userInput.charAt(7));
+                Task completedTask = taskArr.get(taskNum-1);
+                completedTask.markAsUndone();
+                System.out.println("OK, I've marked this task as not done yet:\n"
+                        + " [ ] " + completedTask);
             } else {
-                strArr.add(userInput);
+                taskArr.add(new Task(userInput));
                 System.out.println("Added: " + userInput);
             }
         }
