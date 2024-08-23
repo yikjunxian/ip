8b065c9a66ab800d6189a9a0f915f03d63a94be9
yikjunxian <>
2024-08-23 15:38:49 +0800

    Add support for echo

--- src/main/java/Gumbo.java
+++ src/main/java/Gumbo.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
+
 public class Gumbo {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Gumbo\n" +
-                "What can I do for you?\n" +
-                "Bye. Hope to see you again soon!");
+                "What can I do for you?\n");
+
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        while (true) {
+            String userInput = myObj.nextLine();  // Read user input
+            if (userInput.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            } else {
+                System.out.println(userInput);
+            }
+        }
     }
 }
