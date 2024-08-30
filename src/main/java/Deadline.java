ec282c3de65267bb166f8740dbf25c57968e5146
yikjunxian <>
2024-08-30 14:19:03 +0800

    Add support for dates and times in Deadline

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,19 +1,31 @@
-public class Deadline extends Task {
-    protected String by;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
-    public Deadline(String description, String by) {
+public class Deadline extends Task {
+    protected LocalDate deadline;
+    DateTimeFormatter inputDateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    DateTimeFormatter outputDateFormat = DateTimeFormatter.ofPattern("MMM dd yyyy");
+
+
+    public Deadline(String description, String deadline) {
         super(description);
-        this.by = by;
+        try {
+            this.deadline = LocalDate.parse(deadline, inputDateFormat);
+        } catch (DateTimeParseException e) {
+            System.out.println("Invalid date format. Please input date format as [yyyy-MM-dd].");
+        }
     }
 
     @Override
     public String toTextString() {
         int status = super.isDone ? 1 : 0;
-        return "D" + "," + status + "," + super.description + "," + this.by;
+        return "D" + "," + status + "," + super.description + ","
+                + deadline.format(outputDateFormat);
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: " + deadline.format(outputDateFormat) + ")";
     }
 }
