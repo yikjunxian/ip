44e1508eea95a22b227cc12ef30d699477573f9d
yikjunxian <>
2024-08-30 11:47:09 +0800

    Add implementation of toTextString method

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -10,6 +10,12 @@ public class Event extends Task {
     }
 
     @Override
+    public String toTextString() {
+        int status = super.isDone ? 1 : 0;
+        return "E" + "," + status + "," + super.description + "," + this.from + "," + this.to;
+    }
+
+    @Override
     public String toString() {
         return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
     }
