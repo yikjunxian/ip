d89bd6332ec2539cbe93c28ae9f8fb5a846806de
yikjunxian <>
2024-08-30 11:47:19 +0800

    Add implementation of toTextString method

--- src/main/java/Todo.java
+++ src/main/java/Todo.java
@@ -4,6 +4,12 @@ public class Todo extends Task {
     }
 
     @Override
+    public String toTextString() {
+        int status = super.isDone ? 1 : 0;
+        return "T" + "," + status + "," + super.description;
+    }
+
+    @Override
     public String toString() {
         return "[T]" + super.toString();
     }
