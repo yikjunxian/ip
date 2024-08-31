fa7b234e90b68592a033fd13f3d5304b8b7ca5e2
yikjunxian <>
2024-08-31 15:28:43 +0800

    Add TaskList object that contains the task list and support operations on it

---
+++ src/main/java/tasks/TaskList.java
@@ -0,0 +1,36 @@
+package tasks;
+
+import java.util.ArrayList;
+
+public class TaskList {
+
+    private static ArrayList<Task> taskArr = new ArrayList<>();
+
+    public TaskList(){
+
+    }
+
+    public TaskList(ArrayList<Task> taskArr) {
+        this.taskArr = taskArr;
+    }
+
+    public Task get(int taskNumToGet) {
+        return taskArr.get(taskNumToGet);
+    }
+
+    public ArrayList<Task> getAll() {
+        return taskArr;
+    }
+
+    public void add(Task task) {
+        taskArr.add(task);
+    }
+
+    public void remove(int taskNumToDelete) {
+        taskArr.remove(taskNumToDelete);
+    }
+
+    public int size() {
+        return taskArr.size();
+    }
+}
