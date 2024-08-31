d697ead8d71dada872766ea79e932649332fbc4a
yikjunxian <>
2024-08-31 15:34:41 +0800

    Add IllegalValueException that extends GumboException

--- src/main/java/exceptions/IllegalValueException.java
+++ src/main/java/exceptions/IllegalValueException.java
@@ -1,5 +1,13 @@
-public class GumboException extends Exception {
-    GumboException(String msg) {
-        super(msg);
+package exceptions;
+
+/**
+ * Signals that some given data does not fulfill some constraints.
+ */
+public class IllegalValueException extends GumboException {
+    /**
+     * @param message should contain relevant information on the failed constraint(s)
+     */
+    public IllegalValueException(String message) {
+        super(message);
     }
 }
