cb2e71b0f8803de355202908970b31d5d21c7b3f
yikjunxian <>
2024-09-13 00:24:01 +0800

    Remove redundant run() method

--- src/main/java/gumbo/Gumbo.java
+++ src/main/java/gumbo/Gumbo.java
@@ -32,19 +32,6 @@ public class Gumbo {
         }
     }
 
-    /**
-     * Executes the main logic of the application.
-     */
-//    public void run() {
-//        ui.showWelcome();
-//        boolean isExit = false;
-//        while (!isExit) {
-//            String fullCommand = ui.readCommand();
-//            Command c = Parser.parse(fullCommand);
-//            c.execute(ui, storage, tasks);
-//            isExit = c.isExit();
-//        }
-//    }
     public String getResponse(String input) {
         assert storage != null : "Storage not available";
         assert tasks != null : "Task list not available";
